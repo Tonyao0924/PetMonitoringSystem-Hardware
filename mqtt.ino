8e896b3020e82060c95f311eef2f2705f805bd33
fan9704 <cxz123499@gmail.com>
2023-03-21 19:36:10 +0800

    Complete Send Humidity and Temperature

--- mqtt.ino
+++ mqtt.ino
@@ -1,7 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h> //LiquidCrystal_I2C
+#include <DHT.h>//Adafruit DHT sensor library
+#include <ArduinoJson.h> //ArduinoJson
+#include <TimeLib.h> //Time
 
 // WiFi網絡參數
 const char* ssid = "wander_one";
@@ -12,7 +15,8 @@ const char* machineID = "TestD1";
 const char* mqtt_server = "140.125.207.230";
 const char* mqtt_user = "guest";
 const char* mqtt_password = "guest";
-const char* mqtt_topic = "weightTopic";
+//const char* mqtt_topic = "weightTopic";
+
 // 設定 LCD 顯示器的 I2C 位址和列數、行數
 LiquidCrystal_I2C lcd(0x27, 16, 2);//SDA=D1腳位 SCL=D2腳位
 
@@ -21,6 +25,10 @@ const int buzzerPin = D6;  // 設定蜂鳴器的腳位為 D6
 // 超聲波感測器連接引腳
 const int trigPin = D8;
 const int echoPin = D7;
+
+#define DHTPIN D4     // 感測器接口鍵腳為D4
+#define DHTTYPE DHT11   // 感測器型號為DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 // 建立WiFi客戶端和MQTT客戶端
 WiFiClient espClient;
@@ -43,48 +51,109 @@ long calculateDistance() {
   
   return distance;
 }
-void showDistanceOnLCD(char* distance){
-    // 清空 LCD 顯示器
+void showConnectEroorOnLCD(){
   lcd.clear();
-  // 設定游標位置為第一列第一個位置
   lcd.setCursor(0, 0);
-  // 在 LCD 顯示器上顯示文字
+  lcd.print("AP Connect Error");
+  lcd.setCursor(0, 1);
+  lcd.print("Retype AP Info");
+  delay(2000); 
+}
+
+void showDistanceOnLCD(){
+  const char* mqtt_topic = "distance/";
+  char mqttFullTopic[40]; // 定義一個字符數組用來存儲結果
+  strcpy(mqttFullTopic, mqtt_topic);
+  strcat(mqttFullTopic, machineID); 
+  long distance = calculateDistance();
+  char distanceStr[30];
+  if(distance <=50){
+    Serial.print("\nToo Closed\n");
+    sprintf(distanceStr, "%ld", distance);
+    tone(buzzerPin, 1000);  // 發送 1000 Hz 的聲音信號
+    delay(1000);  // 持續發送聲音信號的時間，這裡是 0.5 秒
+    noTone(buzzerPin);  // 停止發送聲音信號
+  }else{
+    sprintf(distanceStr, "%ld", distance);
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print("Distance:");
   lcd.print(distance);
-  // 設定游標位置為第二列第一個位置
   lcd.setCursor(0, 1);
-  // 在 LCD 顯示器上顯示文字
   lcd.print("I am an Arduino.");
-   // 等待 2 秒
+  
+  //傳送MQTT訊號
+  time_t now = time(nullptr);// Get current time
+  char date[11];
+  sprintf(date, "%04d-%02d-%02d", year(now), month(now), day(now));
+  // 創建一個JSON物件並填充數據
+  DynamicJsonDocument doc(256);
+  doc["Distance"] = distance;
+  doc["Time"] = date;
+  // 將JSON物件轉換成字串
+  String json_str;
+  serializeJson(doc, json_str);
+  // 發送MQTT消息
+  client.publish(mqttFullTopic, json_str.c_str());
+  delay(2000);// 等待 2 秒
+}
+void showTemperatureAndHumidityOnLCD(){
+  const char* mqtt_topic = "temperature/";
+  char mqttFullTopic[40]; // 定義一個字符數組用來存儲結果
+  strcpy(mqttFullTopic, mqtt_topic);
+  strcat(mqttFullTopic, machineID); 
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+    // 檢查是否成功讀取數據
+  if (isnan(temperature) || isnan(humidity)) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temperature:");
+    lcd.print("None");
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity:");
+    lcd.print("None");
+  }else{
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temperature:");
+    lcd.print(temperature);
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity:");
+    lcd.print(humidity);
+  }
+  //傳送MQTT訊號
+  time_t now = time(nullptr);// Get current time
+  char date[11];
+  sprintf(date, "%04d-%02d-%02d", year(now), month(now), day(now));
+  // 創建一個JSON物件並填充數據
+  DynamicJsonDocument doc(256);
+  doc["Temperature"] = temperature;
+  doc["Humidity"] = humidity;
+  doc["Time"] = date;
+  // 將JSON物件轉換成字串
+  String json_str;
+  serializeJson(doc, json_str);
+  // 發送MQTT消息
+  client.publish(mqttFullTopic, json_str.c_str());
+  
   delay(2000);
 }
-void showTemperatureAndHumidityOnLCD(char* distance){
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temperature:");
-  lcd.print(distance);
-  lcd.setCursor(0, 1);
-  lcd.print("Humidity:");
-  lcd.print(distance);
-   // 等待 1 秒
-  delay(1000);
-}
 void setup() {
-  // 初始化串口和GPIO引腳
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  // 將蜂鳴器的腳位設為輸出模式
   pinMode(buzzerPin, OUTPUT);  
-  // 初始化 LCD 顯示器
-  lcd.init();
-  // 打開 LCD 顯示器
-  lcd.backlight();
+  lcd.init();// 初始化 LCD 顯示器
+  lcd.backlight();// 打開 LCD 顯示器
+  dht.begin();
   // 連接WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
+    showConnectEroorOnLCD();
   }
   Serial.println("Connected to WiFi");
 
@@ -104,25 +173,7 @@ void setup() {
 }
 
 void loop() {
-  // 讀取距離
-  long distance = calculateDistance();
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  char distanceStr[30];
-  
-  if(distance <=50){
-    Serial.print("\nToo Closed\n");
-    sprintf(distanceStr, "%ld", distance);
-    tone(buzzerPin, 1000);  // 發送 1000 Hz 的聲音信號
-    delay(500);  // 持續發送聲音信號的時間，這裡是 0.5 秒
-    noTone(buzzerPin);  // 停止發送聲音信號
-  }else{
-    sprintf(distanceStr, "%ld", distance);
-  }
-  showDistanceOnLCD(distanceStr);
-  // 發送MQTT消息
-  client.publish(mqtt_topic, distanceStr);
-
-  // 等待0.5秒
-  delay(500);
+  showDistanceOnLCD();
+  showTemperatureAndHumidityOnLCD();
+  delay(1000);// 等待1秒
 }

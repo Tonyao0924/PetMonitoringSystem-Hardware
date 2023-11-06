8c478532c640ca8b1dca818f3649ae064daad358
fan9704 <cxz123499@gmail.com>
2023-11-06 13:06:40 +0800

    feat: Complete Python MQTT Subscriber

--- mqtttest/subNew.py
+++ mqtttest/subNew.py
@@ -7,9 +7,10 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("weightTopic")
+    client.subscribe("weight/#")
     client.subscribe("temperature/#")
     client.subscribe("humidity/#")
+    client.subscribe("water/#")
     client.subscribe("distance/#")
 
 # The callback for when a PUBLISH message is received from the server.
